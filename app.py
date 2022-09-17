475bd04ee39425cc7e261dfafc05adc18af01eb3
codec404 <saptarshimemari072@gmail.com>
2022-09-17 23:22:02 +0530

    Added rectangle boundary feature

--- app.py
+++ app.py
@@ -5,17 +5,31 @@ import numpy
 import cv2
 import datetime
 from PIL import Image
-url = 'http://172.16.25.244:8080/shot.jpg'
+url = 'http://172.16.14.156:8080/shot.jpg'
+count = 0
 while True:
     image = req.urlopen(url)
     imgBytes = bytearray(image.read())
     imgNumpy = numpy.array(imgBytes , dtype = numpy.uint8)
 
     frame = cv2.imdecode(imgNumpy , -1)
-    cv2.imshow('Smarto Scanner', frame)
-    
-    tm = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+    frame_convert = cv2.cvtColor(frame , cv2.COLOR_BGR2RGB)
+    frame_enhance = cv2.GaussianBlur(frame_convert , (5,5) , 0)
+    frame_edge = cv2.Canny(frame_enhance , 30 , 50)
+    contours , h = cv2.findContours(frame_edge , cv2.RETR_LIST , cv2.CHAIN_APPROX_SIMPLE)
 
-    if cv2.waitKey(1) == ord('s') :
-        imPil = Image.fromarray(frame)
-        imPil.save(f'{tm} Scanned_Document.pdf')
+    if contours :
+        max_cont = max(contours , key = cv2.contourArea)
+        x , y , h , k = cv2.boundingRect(max_cont)
+        cv2.rectangle (frame , (x,y) , (x+h , y+k) , (0,255,255) , 2)
+        try :
+                cv2.imshow('Smarto Scanner', frame)
+                
+                tm = str(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
+
+                if cv2.waitKey(1) == ord('s') :
+                    count += 1
+                    imPil = Image.fromarray(frame_convert)
+                    imPil.save(f'{tm} Scanned_Document_{count}.pdf')
+        except Exception as e :
+                pass
